15cafd2f8dc4e9c24a91f9b3632c10c90f443904
jamilagastar <jamilagastar@gmail.com>
2022-11-14 13:35:58 +1100

    added user controller rspec tests and logic, updated routes and application controller errors and auth

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'shoulda-matchers'
+  gem 'rspec-json_expectations'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,19 @@
 class UsersController < ApplicationController
-    # skip_before_action :authorize, only: :create
+    skip_before_action :authorize, only: :create
 
-    # def create
-    #   user = User.create!(user_params)
-    #   session[:user_id] = user.id
-    #   render json: user, status: :created
-    # end
+    def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+    end
 
-    # def user_params
-    #     params.permit(:first_name, username, :password, :password_confirmation, :phone_number, :email_address, :location, :bio, :image_url)
-    # end
+    def show
+        render json: @current_user
+    end
+
+    private
+    def user_params
+        params.permit(:first_name, :username, :password, :password_confirmation, :phone_number, :email_address, :location, :bio, :image_url)
+    end
 
 end
